59f009c051b9b64515742d4e3c30e3fd637e2c7d
ClemLC <35134833+ClemLC@users.noreply.github.com>
2018-02-16 09:28:59 +0100

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,9 @@
 class HomeController < ApplicationController
   def index
-    end
+  end
 
-    def create
-      SendTweet.new(params[:tweet][:text]).perform
-      redirect_to root_path
-    end
+  def create
+    SendTweet.new(params[:tweet][:text]).perform
+    redirect_to root_path
   end
 end
